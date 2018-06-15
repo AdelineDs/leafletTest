155ea0ec53fdd5e39af2513e793d6b49ffb4f727
Adeline <deca.adeline@gmail.com>
2018-06-15 14:52:13 +0200

    carte leaflet avec photos géolocalisée

---
+++ script.js
@@ -0,0 +1,25 @@
+var france = [48.862725, 2.287592]
+//création de la map
+var mymap = L.map('map').setView(france, 6);
+//creation du calque image
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+            {maxZoom: 18}).addTo(mymap);
+
+ajaxGet("http://localhost/Projets_perso/photosJavascript/photos.json", function (reponse) {
+    var listePhotos = JSON.parse(reponse);
+    var markersCluster = L.markerClusterGroup();
+    for (var i = 0; i < listePhotos.length; i++) {
+        var photo = listePhotos[i];
+        var latLng = new L.LatLng(photo.position.lat, photo.position.lng);
+        var marker = new L.Marker(latLng, {title: photo.nom});
+        markersCluster.addLayer(marker);
+        marker.bindPopup('<h3>' + photo.nom + '</h3>'
+                                    + '<a href="http://localhost/Projets_perso/photosJavascript/img/minou2.JPG" target="_blank"><img alt="' + photo.description + '" src="' + photo.url +'" width="100px" height="auto"></a>'
+                                    + '<p>' + photo.description + '<p>'
+                                    )
+    }
+
+    mymap.addLayer(markersCluster);
+});
+
+
