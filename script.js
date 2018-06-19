33e884c604335d19b5d868c8a7f7148b29d360da
Adeline <deca.adeline@gmail.com>
2018-06-19 20:50:41 +0200

    modif clique marqueur : affichage des autres photos situées dans un rayon de 10km

--- script.js
+++ script.js
@@ -8,18 +8,19 @@ L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/
 ajaxGet("http://localhost/Projets_perso/photosJavascript/photos.json", function (reponse) {
     var listePhotos = JSON.parse(reponse);
     var markersCluster = L.markerClusterGroup();
+    var allPhotos = [];
     for (var i = 0; i < listePhotos.length; i++) {
         let photo = listePhotos[i];
+        allPhotos.push(photo);
         var latLng = new L.LatLng(photo.position.lat, photo.position.lng);
-        var marker = new L.Marker(latLng, {title: photo.nom});
+        let marker = new L.Marker(latLng, {title: photo.nom, alt: latLng});
         markersCluster.addLayer(marker);
-
         marker.on('click', function(){
             var gallery = document.getElementById("gallery")
            if(document.getElementById("thumbnailsLink")){
                gallery.innerHTML="";
            }
-            var lien = document.createElement("a");
+            let lien = document.createElement("a");
             lien.id = "thumbnailsLink";
             lien.href = 'http://localhost/Projets_perso/photosJavascript/' + photo.url;
             
@@ -35,14 +36,72 @@ ajaxGet("http://localhost/Projets_perso/photosJavascript/photos.json", function 
             caption.className = "caption";
             caption.appendChild(document.createTextNode(photo.description));
             gallery.appendChild(caption);
-            
-        
         });
         
-       /* marker.bindPopup('<h3>' + photo.nom + '</h3>'
-                                    + '<a href="http://localhost/Projets_perso/photosJavascript/' + photo.url + '" ><img alt="' + photo.description + '" src="' + photo.url +'" width="100px" height="auto"></a>'
-                                    + '<p>' + photo.description + '<p>'
-                                    )*/
+        var circle = L.circle([48.856614 , 2.352222], {
+            color: 'white',
+            fillColor: '#fff',
+            fillOpacity: 0.5,
+            radius: 10000
+        });
+
+        function onMapClick(e) { 
+            $('h3').hide()
+            $('.none').hide()
+            var currentPos = e.latlng;
+            circle
+                .setLatLng(currentPos)
+                .addTo(mymap);
+            
+            var bounds = circle.getBounds();
+            mymap.fitBounds(bounds);
+
+            var boundsGallery = document.getElementById("boundsGallery")
+            if(document.getElementsByClassName("thumbnailsLink2")){
+                boundsGallery.innerHTML="";
+            }
+            var titre = document.createElement("h3");
+            titre.appendChild(document.createTextNode("Dans le même secteur :"));
+            boundsGallery.appendChild(titre);
+            var texte = document.createElement("p");
+            texte.className = "none";
+            texte.appendChild(document.createTextNode("Aucune suggestion dans cette zone !"));
+            boundsGallery.appendChild(texte);
+                        
+            
+            allPhotos.forEach(function(aPhoto){
+                if(aPhoto.position.lat !== currentPos.lat && aPhoto.position.lng !== currentPos.lng){
+                    if(bounds.contains(aPhoto.position)) {
+                        $('.none').hide();
+                        
+                        var div = document.createElement("div");
+                        div.className = "suggested";
+                        
+                        var lien2 = document.createElement("a");
+                        lien2.className = "thumbnailsLink2";
+                        lien2.href = 'http://localhost/Projets_perso/photosJavascript/' + aPhoto.url;
+                        div.appendChild(lien2);
+
+                        var photoMin2 = document.createElement("img");
+                        photoMin2.className = "thumbnails2";
+                        photoMin2.alt = aPhoto.description;
+                        photoMin2.src = aPhoto.url;
+                        lien2.appendChild(photoMin2);
+
+                        var caption2 = document.createElement("p");
+                        caption2.className = "caption2";
+                        caption2.appendChild(document.createTextNode(aPhoto.description));
+                        div.appendChild(caption2);
+                        
+                        if(document.getElementById("thumbnailsLink").href !== lien2.href ){
+                            boundsGallery.appendChild(div);;
+                        }
+                    }
+            
+                }
+            })
+        }
+        marker.on('click', onMapClick);
     }//end for ----------------------------------
     
     mymap.addLayer(markersCluster);  
@@ -51,6 +110,22 @@ ajaxGet("http://localhost/Projets_perso/photosJavascript/photos.json", function 
 $(document).ready(function() {
     
     $('#gallery').magnificPopup({
+            delegate: "a",
+            type: 'image',
+            closeOnContentClick : true,
+            closeBtnInside : false,
+            fixedContentPos : true,
+            mainClass: 'mfp-no-margins mfp-with-zoom',
+            image: {
+                verticalFit: true
+            },
+            zoom: {
+                enabled: true,
+                duration: 500
+            }
+    });
+    
+    $('#boundsGallery').magnificPopup({
             delegate: "a",
             type: 'image',
             closeOnContentClick : true,
