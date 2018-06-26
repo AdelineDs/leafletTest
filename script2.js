ddeb320cd482f202d4ac84194f5b0201af831681
Adeline <deca.adeline@gmail.com>
2018-06-26 14:46:38 +0200

    ajout de sliders pour l'affichage des images

--- script2.js
+++ script2.js
@@ -53,58 +53,112 @@ class leafletMap{
             
             this.list.on('item-click', e => {
                 let imageGallery = new Gallery(e.layer.gallery);
+                
+                
+                $("#rslides").removeClass();
+                $('.rslides_nav').remove();
+                $("#rslides").responsiveSlides({
+                    auto: true,
+                    speed: 400,
+                    timeout: 3000,
+                    pause: true,
+                    pagination: false,
+                    nav: true,
+                    maxwidth: 800
+                });
+                
+                $('.li').magnificPopup({
+                    delegate: "a",
+                    type: 'image',
+                    closeOnContentClick : true,
+                    closeBtnInside : false,
+                    fixedContentPos : true,
+                    mainClass: 'mfp-no-margins mfp-with-zoom',
+                    image: {
+                        verticalFit: true
+                    },
+                    zoom: {
+                        enabled: true,
+                        duration: 500
+                    }
+                    
+                });
+
+                $('#boundsGallery').multislider({interval: 2000});
+                $('#boundsGallery').magnificPopup({
+                    delegate: "a",
+                    type: 'image',
+                    closeOnContentClick : true,
+                    closeBtnInside : false,
+                    fixedContentPos : true,
+                    mainClass: 'mfp-no-margins mfp-with-zoom',
+                    image: {
+                        verticalFit: true
+                    },
+                    zoom: {
+                        enabled: true,
+                        duration: 500
+                    }
+                    
+                });
+                
                 this.myMap.on('moveend', () => {
+                    $("#boundsGallery").removeClass();
+
                     let bounds =this.myMap.getBounds();
                 
                     e.layer.gallery.forEach(element => {
-
-                        $('h3').hide();
-                        $('.none').hide();
+                        
+                        const text = document.getElementById("text");
+                        $('h3').remove();
+                        $('.empty').remove();
+                        
+                        const title = document.createElement("h3");
+                        title.appendChild(document.createTextNode("Dans le même secteur :"));
+                        text.appendChild(title);
+                        const empty = document.createElement("p");
+                        empty.className = "empty";
+                        empty.appendChild(document.createTextNode("Aucune suggestion dans cette zone !"));
+                        text.appendChild(empty);
+                        
                         const boundsGallery = document.getElementById("boundsGallery");
+                        const slider = document.getElementById("slider");
 
                         if(document.getElementsByClassName("thumbnailsLink2")){
-                            boundsGallery.innerHTML="";
+                            slider.innerHTML="";
+                            $('.MS-controls').hide();
                         }
-
-                        const titre = document.createElement("h3");
-                        titre.appendChild(document.createTextNode("Dans le même secteur :"));
-                        boundsGallery.appendChild(titre);
-                        const texte = document.createElement("p");
-                        texte.className = "none";
-                        texte.appendChild(document.createTextNode("Aucune suggestion dans cette zone !"));
-                        boundsGallery.appendChild(texte);
 
                         photosList.forEach(function(aPhoto){
                             if(aPhoto.position.lat !== element.position.lat && aPhoto.position.lng !== element.position.lng){
                                 if(bounds.contains(aPhoto.position)) {
-                                    $('.none').hide();
+                                    $('.empty').remove();
+                                    $('.MS-controls').show();
 
-                                    var div = document.createElement("div");
-                                    div.className = "suggested";
+                                    const div = document.createElement("div");
+                                    div.className = "item";
 
-                                    var lien2 = document.createElement("a");
+                                    const lien2 = document.createElement("a");
                                     lien2.className = "thumbnailsLink2";
                                     lien2.href = 'http://localhost/Projets_perso/photosJavascript/' + aPhoto.url;
                                     div.appendChild(lien2);
 
-                                    var photoMin2 = document.createElement("img");
+                                    const photoMin2 = document.createElement("img");
                                     photoMin2.className = "thumbnails2";
                                     photoMin2.alt = aPhoto.description;
                                     photoMin2.src = aPhoto.url;
                                     lien2.appendChild(photoMin2);
 
-                                    var caption2 = document.createElement("p");
+                                    const caption2 = document.createElement("p");
                                     caption2.className = "caption2";
                                     caption2.appendChild(document.createTextNode(aPhoto.description));
                                     div.appendChild(caption2);
 
-                                    if(document.getElementById("thumbnailsLink").href !== lien2.href ){
-                                        boundsGallery.appendChild(div);;
-                                    }//-- end if --
+                                    slider.appendChild(div);
                                 }//-- end if --
                             }//-- end if --
                         })//-- end forEach --
-                    })//-- end forEach -- 
+                    })//-- end forEach --
                 })//-- end myMyap.moveend -- 
             })//-- end list.on --
         });//-- end ajaxGet --
@@ -142,14 +196,17 @@ class Gallery {
     }//-- end constructor --
     
     createGallery(source){
-        const gallery = document.getElementById("gallery");
+        const gallery = document.getElementById("rslides");
         if(document.getElementById("thumbnailsLink")){
                    gallery.innerHTML="";
         }
         source.forEach(element => {
+            const li = document.createElement("li")
+            li.className = "li";
+            
             const link = document.createElement("a");
             link.id = "thumbnailsLink";
-            link.href = `http://localhost/Projets_perso/photosJavascript/ ${element.url}`;
+            link.href = `http://localhost/Projets_perso/photosJavascript/${element.url}`;
 
             const thumbnails = document.createElement("img");
             thumbnails.className = "thumbnails";
@@ -157,18 +214,20 @@ class Gallery {
             thumbnails.src =  element.url;
 
             link.appendChild(thumbnails);
-            gallery.appendChild(link);
+            li.appendChild(link);
 
             const caption = document.createElement("p");
             caption.className = "caption";
             caption.appendChild(document.createTextNode(element.description));
-            gallery.appendChild(caption);
+            li.appendChild(caption);
+            gallery.appendChild(li);
         });
-    } 
+    }//-- end createGallery -- 
 }//---- END CLASS GALLERY ----
 
 
-window.onload = function(){
+window.onload = () => {
     let myMap = new leafletMap("map");
     myMap.photoRecovery("http://localhost/Projets_perso/photosJavascript/photos.json");
 }
+
